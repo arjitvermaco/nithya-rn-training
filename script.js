c96a126b80be97bbb9cd9c64deacc165e3d3fe3d
Arjit <arjitverma.go@gmail.com>
2023-09-12 15:44:15 +0530

    JS Fundamentals Day 1

--- script.js
+++ script.js
@@ -190,3 +190,54 @@
 // }else{
 //     console.log("Height is not defined!!!")
 // }
+
+// Equality Operators : == vs ===
+// const age = '18';
+// if(age === 18) console.log('You just became an adult :D (strict)')
+
+// if(age == 18) console.log('You just became an adult :D (loose)')
+
+// let userAge = Number(prompt("What is your age??"));
+// console.log(userAge)
+// console.log(typeof userAge)
+
+const day = 'saturday';
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+  } else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+  } else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples');
+  } else if (day === 'friday') {
+    console.log('Record videos');
+  } else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend :D');
+  } else {
+    console.log('Not a valid day!');
+  }
+
+switch (day) {
+    case 'monday': // day === 'monday'
+      console.log('Plan course structure');
+      console.log('Go to coding meetup');
+      break;
+    case 'tuesday':
+      console.log('Prepare theory videos');
+      break;
+    case 'wednesday':
+    case 'thursday':
+      console.log('Write code examples');
+      break;
+    case 'friday':
+      console.log('Record videos');
+      break;
+    case 'saturday':
+    case 'sunday':
+      console.log('Enjoy the weekend :D');
+      break;
+    default:
+      console.log('Not a valid day!');
+}
+  
+  
